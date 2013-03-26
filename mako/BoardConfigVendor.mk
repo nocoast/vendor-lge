62d80f25d24461c2c2547f9d279657bfb8440e09
drcmda <drcmda@gmail.com>
2013-03-26 11:21:30 +0000

    mako widevine so's
    Change-Id: Ie4778e111551dd2aaefdda430b250f7ad9f226c1

--- mako/BoardConfigVendor.mk
+++ mako/BoardConfigVendor.mk
@@ -17,3 +17,4 @@ LOCAL_STEM := mako/BoardConfigPartial.mk
 -include vendor/broadcom/$(LOCAL_STEM)
 -include vendor/lge/$(LOCAL_STEM)
 -include vendor/qcom/$(LOCAL_STEM)
+-include vendor/widevine/$(LOCAL_STEM)
